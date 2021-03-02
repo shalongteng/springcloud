9847b3affd69920d931ed5b14b8f97db41c93ec5
shalongteng <1131790681@qq.com>
2021-03-02 21:45:30 +0800

    分布式事务

---
+++ cloud-admin/src/test/java/com/slt/taxi/cloudadmin/CloudAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slt.taxi.cloudadmin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- consumer/src/main/java/com/slt/consumer/remote/HelloRemote.java
+++ consumer/src/main/java/com/slt/consumer/remote/HelloRemote.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "spring-cloud-provider",configuration = FeignAuthConfiguration.class,fallbackFactory = HelloRemote.FeignClientFallbackFactory.class)
+//@FeignClient(name = "spring-cloud-provider",configuration = FeignAuthConfiguration.class,fallbackFactory = HelloRemote.FeignClientFallbackFactory.class)
+@FeignClient(name = "spring-cloud-provider",fallbackFactory = HelloRemote.FeignClientFallbackFactory.class)
 public interface HelloRemote {
     @RequestMapping(value = "/hello/{name}")
     public String hello(@PathVariable("name") String name);
